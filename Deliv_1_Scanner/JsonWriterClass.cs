e68fead7fd0597faae0093f982f5b88afedc17b4
Dylan_Carder <123211944+I-am-Signal@users.noreply.github.com>
2023-12-31 13:00:46 -0500

    Deliv_1_Scanner Code and Executable

---
+++ Deliv_1_Scanner/JsonWriterClass.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Kennesaw State University
+ * College of Computer and Software Engineering
+ * Department of Computer Science
+ * CS 4308, Concepts of Programming Languages, Section W02
+ * Project 1st Deliverable
+ * Connor Bell, Dylan Carder, Sebastian Utz, Kevin Vu
+ * Program: JsonWriteClass.cs
+ * September 24, 2023
+*/
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+// JsonWriterClass Class
+public class JsonWriterClass
+{
+    // Declared StreamWriter to write to a file
+    StreamWriter JsonWriter;
+    List<Token> TokenList;
+
+    /// <summary>
+    /// Create JsonWriterClass object, writing the JSON file from the FinalTokenList at the selected Filename
+    /// </summary>
+    /// <param name="Filename"></param>
+    /// <param name="FinalTokenList"></param>
+    public JsonWriterClass(string Filename, List<Token> FinalTokenList)
+    {
+        JsonWriter = new StreamWriter(Filename, true);
+        this.TokenList = FinalTokenList;
+        WriteJsonFile();
+    }
+
+    //WriteJsonFile Method
+    private void WriteJsonFile()
+    {
+        try
+        {
+            if (TokenList.Count > 0) //Checks TokenList is empty
+            {
+                //Writes to file in JSON format
+                JsonWriter.WriteLine("{");
+                int TokenCounter = 0;
+                foreach (Token T in TokenList) //Loop to write each token to the file
+                {
+                    JsonWriter.WriteLine("  \"Token_" + TokenCounter + "\": {");
+                    JsonWriter.WriteLine("    \"Type\": \"" + T.GetTypeName() + "\",");
+                    JsonWriter.WriteLine("    \"ID\": \"" + T.GetID() + "\",");
+                    JsonWriter.WriteLine("    \"Value\": \"" + T.GetValue() + "\",");
+                    if (TokenCounter == TokenList.Count - 1) JsonWriter.WriteLine("  }");
+                    else JsonWriter.WriteLine("  },");
+                    TokenCounter++;
+                }
+                JsonWriter.WriteLine("}");
+            }
+            else throw new Exception("TokenList is empty");
+        }
+
+        catch (Exception e)
+        { 
+            Console.WriteLine("Error: " + e.Message); 
+        }
+        
+        JsonWriter.Close(); //SAVE THOSE RESOURCES
+    }
+}
---
+++ Deliv_1_Scanner/MainClass.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Kennesaw State University
+ * College of Computer and Software Engineering
+ * Department of Computer Science
+ * CS 4308, Concepts of Programming Languages, Section W02
+ * Project 1st Deliverable
+ * Connor Bell, Dylan Carder, Sebastian Utz, Kevin Vu
+ * Program: MainClass.cs
+ * September 24, 2023
+*/
+
+using System;
+using System.Collections.Generic;
+
+// MainClass Class
+public class MainClass
+{
+    static SCLScanner Scanner = new SCLScanner();
+    // TestWithFilepath Method (used for testing in IDE without command line compilation)
+    public static void TestWithFilePath()
+    {
+        // Prompt user, record file path, begin scan
+        Console.Write("Filepath: ");
+        string Filepath = Console.ReadLine();
+        if (Filepath == null)
+        {
+            Console.WriteLine("Error: No file path specified");
+            return;
+        }
+        Scanner.Scan(Filepath);
+    }
+
+    // Main Method
+    public static void Main(string[] args)
+    {
+        // If a command line argument is specified, begin scan
+        if (args.Length != 0)
+        {
+            string Path = args[0];
+            Scanner.Scan(Path);
+        } 
+        else Console.WriteLine("Error: No file path specified.");
+        //TestWithFilePath();
+    }
+}
